63ce598ef3bbd09a0ab2cb9ec8d4424d4f442145
wilembergson <wilembergson@yahoo.com>
2024-03-11 21:33:15 -0300

    feat:mudar status do pedido

--- app/components/Pedido.tsx
+++ app/components/Pedido.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import {PedidoTipo} from '../page'
 import EditarItemModal from './EditarItemModal'
 import Item from './Item'
@@ -6,18 +6,29 @@ import { erroMessage, sucessMessage } from '../utils/Toasts'
 import api from '../api/api-connection'
 import { FaEdit } from "react-icons/fa"
 import { FaCheckCircle } from "react-icons/fa"
+import { MdDeleteForever } from 'react-icons/md'
+import { FaCheck } from "react-icons/fa"
+import { MdDeliveryDining } from "react-icons/md"
+import { LuPackageCheck } from "react-icons/lu";
 
 type Props = {
     dados:PedidoTipo
 }
 
 export default function Pedido({dados}:Props){
+    const inputRef = useRef<HTMLInputElement>(null)
     const [editando, setEditando] = useState(false)
     const [pix, setPix] = useState<string>(dados.pix)
 
     function handleChange(e: any) {
         e.preventDefault()
         setPix((e.target.value))
+    }
+
+    function editar(){
+       
+        setEditando(true)
+        
     }
 
     async function atualizar(){
@@ -30,12 +41,18 @@ export default function Pedido({dados}:Props){
         }
     }
 
+    useEffect(() => {
+        if (inputRef.current) {
+            inputRef.current.focus()
+        }
+    }, [editando])
+
     return(
-            <div className='flex flex-col p-2 m-4 md:w-4/5 w-full rounded-md text-gray-clear'>
-                <div className='flex flex-col bg-white shadow-md rounded-md w-full p-2 mb-4'>
-                    <div className='flex justify-between items-center'>
+            <div className='flex flex-col p-2 w-full rounded-md'>
+                <div className='flex flex-col bg-white text-azul shadow-md rounded-md w-full p-2 mb-4'>
+                    <div className='flex flex-wrap mb-4 justify-start items-center'>
                         {editando ?
-                            <input className='flex mb-4 bg-gray-300 p-2 rounded-md'
+                            <input ref={inputRef} className='flex mr-2 bg-gray-300 p-2 rounded-md'
                                 type="text"
                                 placeholder='pix'
                                 name='pix'
@@ -44,20 +61,37 @@ export default function Pedido({dados}:Props){
                                 required
                             />
                         :
-                            <h1 className='flex text-2xl mb-4 font-bold'>
+                            <h1 className='flex text-lg mr-2 font-bold'>
                                 PIX: {dados?.pix}
                             </h1>
                         } 
                         {editando ?
                             <button onClick={() => atualizar()}>
-                                <FaCheckCircle size={30}/>
+                                <FaCheckCircle size={30} color='#F5B041'/>
                             </button>    
                         :
-                            <button onClick={() => setEditando(true)}>
-                                <FaEdit size={30}/>
+                            <button onClick={() => editar()}>
+                                <FaEdit size={30} color='#F5B041'/>
                             </button>
                         }
                     </div>
+                    <section className='flex flex-col'>
+                        <h1>STATUS: <strong>{dados.status}</strong></h1>
+                        <div className='flex flex-wrap mb-4'>
+                            <div className="flex items-center bg-blue-600
+                                    text-white rounded-md shadow-md p-1 mr-2">
+                                <FaCheck size={14}/> Feito
+                            </div>
+                            <div className="flex bg-orange-600 
+                                    text-white rounded-md shadow-md p-1 mr-2">
+                                <MdDeliveryDining size={24}/> <h1>À caminho</h1>
+                            </div>
+                            <div className="flex items-center bg-green-600
+                                    text-white rounded-md shadow-md p-1">
+                                <LuPackageCheck size={24}/> Entregue
+                    </div>
+                        </div>
+                    </section>
                     <section className='flex justify-between w-full'>
                         <div>
                             <h2 className='text-red-700'>Faltam pagar: R${dados.totalAPagar.toFixed(2)}</h2>
